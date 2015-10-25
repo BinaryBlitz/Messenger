55265cde0ddc41d6030714c00f19c3b5d4b66fb4
andreymikhaylov <drumih@gmail.com>
2015-10-25 21:47:21 +0300

    port changed

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride());
+
+app.set('port', (process.env.PORT || 5000));
 //app.use(express.static(path.join(__dirname, "public")));
 
 
@@ -180,5 +182,7 @@ io.on('connection', function(socket) {
 });
 /*||||||||||||||||||||||||||||||||||||||END SOCKETS||||||||||||||||||||||||||||||||||||||*/
 
-server.listen(2015);
-console.log('It\'s going down in 2015');
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+// console.log('It\'s going down in 2015');
