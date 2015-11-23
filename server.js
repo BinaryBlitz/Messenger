08466d636a479f8f183ead0e19658fc288ef6bf7
andreymikhaylov <drumih@gmail.com>
2015-11-23 13:25:48 +0300

    server changed

--- server.js
+++ server.js
@@ -14,19 +14,11 @@ var server = app.listen(app.get('port'), function () {
 
 var io = require('socket.io')(server);
 
-//Set our static file directory to public
-//app.use(express.static(__dirname + '/public'));
-
-
 app.use(favicon(__dirname + '/favicon.ico'));
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride());
-
-//app.set('port', (process.env.PORT || 80));
-//app.use(express.static(path.join(__dirname, "public")));
-
 
 //Connect to mongo DB database
 
@@ -50,22 +42,18 @@ var ChatSchema = mongoose.Schema({
   created: Date,
   content: String,
   username: String,
-  room: String
+  room: String,
+  from_id:Number
 });
 
 var ConversationSchema = mongoose.Schema({
-
 	users:[Number],
 	messages:[ChatSchema]
-
 });
 
 var UserSchema = mongoose.Schema({
-
 	userID:Number,
 	username:String,
-	conversations:[ConversationSchema]
-
 });
 
 
@@ -89,88 +77,21 @@ app.all('/*', function(req, res, next) {
 /*||||||||||||||||||||||||||||||||||||||ROUTES||||||||||||||||||||||||||||||||||||||*/
 //Route for our index file
 app.get('/', function(req, res) {
-  //send the index.html in our public directory
- // res.sendfile('index.html');
  res.json({"main":"true"});
 });
 
-
-//This route is simply run only on first launch just to generate some chat history
-// app.post('/setup', function(req, res) {
-//   //Array of chat data. Each object properties must match the schema object properties
-//   var chatData = [{
-//     created: new Date(),
-//     content: 'Hi',
-//     username: 'Chris',
-//     room: 'php'
-//   }, {
-//     created: new Date(),
-//     content: 'Hello',
-//     username: 'Obinna',
-//     room: 'laravel'
-//   }, {
-//     created: new Date(),
-//     content: 'Ait',
-//     username: 'Bill',
-//     room: 'angular'
-//   }, {
-//     created: new Date(),
-//     content: 'Amazing room',
-//     username: 'Patience',
-//     room: 'socket.io'
-//   }];
-
-//   //Loop through each of the chat data and insert into the database
-//   for (var c = 0; c < chatData.length; c++) {
-//     //Create an instance of the chat model
-//     var newChat = new Chat(chatData[c]);
-//     //Call save to insert the chat
-//     newChat.save(function(err, savedChat) {
-//       console.log(savedChat);
-//     });
-//   }
-//   //Send a resoponse so the serve would not get stuck
-//   res.json({message: 'Point created!'});
-//  // res.send('created');
-// });
-
-//This route produces a list of chat as filterd by 'room' query
-// app.get('/msg', function(req, res) {
-//   //Find
-//   Chat.find({
-//     'room': req.query.room.toLowerCase()
-//   }).exec(function(err, msgs) {
-//     //Send
-//     res.json(msgs);
-//   });
-// });
-
-// app.post('/users/registre', function(req,res){
-// 	var user_id = req.query.user_id;
-// 	var username = req.query.username;
-// 	User.findOne({"userID":user_id}, function(err,user){
-// 	if(err) {
-// 		var u = new User({userID:user_id, username:username,conversations:[]});
-// 		u.save(function(err,usr){
-// 			if(!err){
-// 			res.json({"user":usr});
-// 		} else {
-// 			res.json(err);
-// 		}
-// 		});
-// 	} else {
-// 		res.json({"user":user});
-// 	}
-// 	});
-// });
-
-//TODO
 app.get('/messages',function(req,res) {
 
 	var from_id = req.query.from_id;
 	var to_id = req.query.to_id;
 
-
+	findConvFor(from_id,to_id,function(err, conv) {
+		if(!err){
+		res.json(conv.messages);
+	} else {
+		res.json(err);
+	}
+	});
 
 });
 
@@ -196,36 +117,6 @@ app.post('/conversations', function(req, res){
 	var message = req.body.message;
 	var to_id = req.body.to_id;
 	var from_id = req.body.from_id;
-
-	// findConvFor(from_id, to_id, function(err, conv){
-	// 	if(conv){
-	// 		saveMessage(message,function(err,msg){
-	// 			conv.messages.push(msg);
-	// 			conv.save(function(err,converss){
-	// 				res.json(converss);
-	// 			});
-	// 		});
-	// 	} else {
-	// 		saveMessage(message, function(err, msg){
-
-	// 		conv = new Conversation ({users : [to_id,from_id],
-	// 										messages :[msg]
-	// 									});
-
-	// 		conv.save(function(error,convers){
-	// 			if(error){
-	// 				res.json(error);
-	// 			} else {
-	// 				saveConversation(convers, to_id);
-	// 				saveConversation(convers, from_id);
-	// 				res.json(convers);
-	// 			}
-	// 		});
-	// 	});
-	// 	}
-	// }
-
-	// );
 
 	messageWork(message, from_id,to_id, function(err, conv){
 		if(err){
@@ -270,11 +161,12 @@ var findConvFor = function(from_id, to_id,next){
 }
 
 var saveMessage = function (data, next) {
-	console.log(data);
+	//console.log(data);
 	var newMsg = new Chat({
    				   username: data.username,
       				content: data.message,
-     				 created: new Date() 
+     				 created: new Date(),
+     				 from_id: data.from_id  
      	      });
 	newMsg.save(function(err,msg){
 		next(err,msg);
@@ -298,55 +190,28 @@ var saveConversation = function (conversation, user_id) {
 	});
 }
 
-// app.post('/conversations', function(req, res) {
-// 	var from_id = req.query.from_id;
-// 	var to_id = req.query.to_id;
-
-// 	Conversation.find($or:[[]])
-
-// }
-// );
 /*||||||||||||||||||||||||||||||||||||||END ROUTES||||||||||||||||||||||||||||||||||||||*/
 
 /*||||||||||||||||||||||||||||||||||||||SOCKET||||||||||||||||||||||||||||||||||||||*/
 //Listen for connection
 io.on('connection', function(socket) {
-  //Globals
-  // var defaultRoom = 'general';
-  // var rooms = ["General", "angular", "socket.io", "express", "node", "mongo", "PHP", "laravel"];
 
-
-  //Emit the rooms array
   socket.emit('setup', {
     status:"connected"
   });
 
-  socket.on('new user', function(data) {
-  	socket.join(data.user_id);
-  	socket.in(data.user_id).emit({status:'joined'});
-  });
+  socket.on('new user', function (data){
 
-  //Listens for new user
-  // socket.on('new user', function(data) {
-  //   data.room = defaultRoom;
-  //   //New user joins the default room
-  //   socket.join(defaultRoom);
-  //   //Tell all those in the room that a new user joined
-  //   io.in(defaultRoom).emit('user joined', data);
-  // });
+  	var user_id = data.user_id;
 
-  //Listens for switch room
-  // socket.on('switch room', function(data) {
-  //   //Handles joining and leaving rooms
-  //   //console.log(data);
-  //   socket.leave(data.oldRoom);
-  //   socket.join(data.newRoom);
-  //   io.in(data.oldRoom).emit('user left', data);
-  //   io.in(data.newRoom).emit('user joined', data);
+  	console.log(user_id);
 
-  // });
+  	socket.join(user_id);
 
-  //Listens for a new chat message
+  }); 
+
+
+
   socket.on('new message', function(data) {
 
   	var message = data.message;
@@ -354,32 +219,19 @@ io.on('connection', function(socket) {
 	var from_id = data.from_id;
 
 console.log (message);
+console.log(to_id, from_id);
 	messageWork(message,to_id,from_id, function(err, conv){
 		if(conv){
+			//socket.join(to_id);
+			//socket.join(from_id);
+
 			var msg = conv.messages[conv.messages.length - 1];
 			io.in(to_id).emit('message created', msg);
 			io.in(from_id).emit('message created', msg);
 		}
 	});
-    //Create message
-    // var newMsg = new Chat({
-    //   username: data.username,
-    //   content: data.message,
-    //   room: data.room.toLowerCase(),
-    //   created: new Date()
-    // });
-    // //Save it to database
-    // newMsg.save(function(err, msg){
-    //   //Send message to those connected in the room
-    //   io.in(msg.room).emit('message created', msg);
-    // });
-
 
   });
 });
 /*||||||||||||||||||||||||||||||||||||||END SOCKETS||||||||||||||||||||||||||||||||||||||*/
 
-// app.listen(app.get('port'), function () {
-//     console.log('Express server listening on port ' + app.get('port'));
-// });
-// console.log('It\'s going down in 2015');
